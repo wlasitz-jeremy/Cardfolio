c88125b169bb7e062e5ea412cc9588c7f53130a9
wlasitz-jeremy <wlasitz@gmail.com>
2026-09-14 16:27:29 -0600

    added color pallette and began adding typography

---
+++ src/theme/colors.ts
@@ -0,0 +1,22 @@
+export const colors = {
+    
+    // Base Colors
+    LightBase: 'f4f4f8',
+    DarkBase: '111315',
+    
+    // Structural Panels
+    LightStructuralPanels: 'e2e1f4',
+    DarkStructuralPanels: '1f1e27',
+    
+    // Main Typography
+    LightMainTypography: '1e293b',
+    DarkMainTypography: 'f1f3f5',
+    
+    // Secondary Typography
+    LightSecondaryTypography: '424b57',
+    DarkSecondaryTypography: 'c4c7ca',
+    
+    // Accent Colors
+    LightAccent: '4a475c',
+    DarkAccent: 'c1bbe1',
+};
